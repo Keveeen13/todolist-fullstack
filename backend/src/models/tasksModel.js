201127c458e93b1abacedbcecf78cd72fb2f5938
Kéven Patrício <kevenprogramas@gmail.com>
2024-10-08 17:37:20 -0300

    Models

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const getALL = async () => {
     const [tasks] = await connection.execute('SELECT * FROM tasks');
-    return tasks[0];
+    return tasks;
 };
 
 const createTask = async (task) => {
@@ -12,11 +12,26 @@ const createTask = async (task) => {
     const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)';
 
     const [createdTask] = await connection.execute(query, [title, 'pendente', dateUTC]);
+    return {insertId: createdTask.insertId};
+};
 
-    return createdTask;
+const deleteTasks = async (id) => {
+    const [removedTask] = await connection.execute('DELETE FROM tasks WHERE id = ?', [id]);
+    return removedTask;
+}
+
+const updateTasks = async (id, task) => {
+    const { title, status } = task;
+    
+    const query = 'UPDATE tasks SET title = ?, status = ? WHERE id = ?';
+
+    const [updatedTask] = await connection.execute(query, [title, status, id]);
+    return updatedTask; 
 }
 
 module.exports = {
     getALL,
     createTask,
+    deleteTasks,
+    updateTasks,
 };
