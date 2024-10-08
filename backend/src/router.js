dd5610340e119f489099bfb3752c6eb50a600c36
Kéven Patrício <kevenprogramas@gmail.com>
2024-10-08 17:37:54 -0300

    Rotas

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
 const tasksController = require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddleware');
 
 const router = express.Router();
 
 router.get('/tasks', tasksController.getALL);
-router.post('/tasks', tasksController.createTask);
+router.post('/tasks', tasksMiddleware.validateFieldTitle, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTasks);
+router.put('/tasks/:id',
+    tasksMiddleware.validateFieldTitle,
+    tasksMiddleware.validateFieldStatus,
+    tasksController.updateTasks,
+);
 
 module.exports = router;
